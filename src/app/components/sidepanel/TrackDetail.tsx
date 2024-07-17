9991a6e4d6bb6387d07110dfa5800904904ac4b9
Damien Vantourout <damien.vantourout@protonmail.ch>
2024-07-17 10:37:24 +0200

    add responsive

--- src/app/components/sidepanel/TrackDetail.tsx
+++ src/app/components/sidepanel/TrackDetail.tsx
@@ -1,46 +1,72 @@
-import { ReactNode } from "react";
+import { ReactNode } from 'react'
 
-import { geoJSON, LatLng, Map } from "leaflet";
-import { useSetRecoilState } from "recoil";
+import { geoJSON, LatLng, Map } from 'leaflet'
+import { useSetRecoilState } from 'recoil'
 
-import { sidePanelAtom } from "@/app/atoms";
-import { Track } from "@/app/types";
+import { sidePanelAtom } from '@/app/atoms'
+import { Track } from '@/app/types'
 
-const TrackInformation: React.FC<{children?: ReactNode}> = ({children}) => {
-  return <div className="h-8">{children}</div>
+const TrackInformation: React.FC<{ children?: ReactNode }> = ({ children }) => {
+    return <div className="h-8">{children}</div>
 }
 
 const TrackDetail: React.FC<{ selectedGpx: Track; map: Map }> = ({
-  selectedGpx,
-  map,
+    selectedGpx,
+    map,
 }) => {
-  const setSidePanel = useSetRecoilState(sidePanelAtom)
+    const setSidePanel = useSetRecoilState(sidePanelAtom)
 
-  return (
-    <>
-      <div className="h-8 font-bold">{selectedGpx.properties.name}</div>
-      <TrackInformation>Distance: {Math.round(selectedGpx.properties.distance) / 1000}km</TrackInformation>
-      <TrackInformation>Elevation gain: {Math.round(selectedGpx.properties.elevation_gain)}m</TrackInformation>
-      <TrackInformation>Elevation loss: {Math.round(selectedGpx.properties.elevation_loss)}m</TrackInformation>
-      <button
-        onClick={() => {
-          setSidePanel(() => false);
-          
-          map.flyToBounds(
-            geoJSON(selectedGpx.geometry, {
-              coordsToLatLng: (coords) => {
-                return new LatLng(coords[0], coords[1], coords[2]);
-              },
-            })
-              .getBounds()
-              .pad(0.1)
-          );
-        }}
-      >
-        Go to
-      </button>
-    </>
-  );
-};
+    return (
+        <div className="p-4">
+            <TrackInformation>
+                Distance: {Math.round(selectedGpx.properties.distance) / 1000}km
+            </TrackInformation>
+            <TrackInformation>
+                Elevation gain:{' '}
+                {Math.round(selectedGpx.properties.elevation_gain)}m
+            </TrackInformation>
+            <TrackInformation>
+                Elevation loss:{' '}
+                {Math.round(selectedGpx.properties.elevation_loss)}m
+            </TrackInformation>
+            <button
+                onClick={() => {
+                    setSidePanel(() => false)
 
-export default TrackDetail;
+                    map.flyToBounds(
+                        geoJSON(selectedGpx.geometry, {
+                            coordsToLatLng: (coords) => {
+                                return new LatLng(
+                                    coords[0],
+                                    coords[1],
+                                    coords[2]
+                                )
+                            },
+                        })
+                            .getBounds()
+                            .pad(0.1)
+                    )
+                }}
+                className="flex text-blue-400 ml-auto"
+            >
+                Show on map
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                    />
+                </svg>
+            </button>
+        </div>
+    )
+}
+
+export default TrackDetail
--- src/app/components/sidepanel/index.tsx
+++ src/app/components/sidepanel/index.tsx
@@ -1,49 +1,130 @@
-"use client";
+'use client'
 
-import { Map } from "leaflet";
-import { useRecoilState } from "recoil";
+import { Map } from 'leaflet'
+import { useRecoilState } from 'recoil'
 
-import { selectedGpxAtom, sidePanelAtom } from "@/app/atoms";
-import { TracksByRegion } from "@/app/types";
+import { selectedGpxAtom, sidePanelAtom } from '@/app/atoms'
+import { TracksByRegion } from '@/app/types'
 
-import GPXList from "./GPXList";
-import TrackDetail from "./TrackDetail";
-
+import TrackDetail from './TrackDetail'
+import TracksList from './TracksList'
 
 const SidePanel: React.FC<{ tracks: TracksByRegion; map: Map }> = ({
-  tracks,
-  map,
+    tracks,
+    map,
 }) => {
-  const [sidePanel, setSidePanel] = useRecoilState(sidePanelAtom);
-  const [selectedGpx, setSelectedGpx] = useRecoilState(selectedGpxAtom);
+    const [sidePanel, setSidePanel] = useRecoilState(sidePanelAtom)
+    const [selectedGpx, setSelectedGpx] = useRecoilState(selectedGpxAtom)
 
-  return (
-    <div
-      className={`absolute sm:w-[408px] h-screen bg-white z-[1001] transition-transform ${
-        sidePanel ? "-translate-x-0" : "-translate-x-full"
-      } w-full`}
-    >
-      <div className="overflow-scroll scrollbar-width-thin h-screen">
-        {selectedGpx ? (
-          <div className="p-4">
-            <button onClick={() => setSelectedGpx(() => null)}>Back</button>
-            <TrackDetail selectedGpx={selectedGpx} map={map} />
-          </div>
-        ) : (<>
-        <button className="sm:hidden" onClick={() => setSidePanel(() => false)}>Back</button>
-          <GPXList tracks={tracks} /></>
-        )}
-      </div>
-      <div
-        className={`sm:block absolute left-full sm:left-[408px] top-1/2 w-6 h-12 bg-white z-[401] leading-[48px] text-center ${
-          !sidePanel ? "transform -scale-x-100" : "hidden"
-        }` }
-        onClick={() => setSidePanel((v) => !v)}
-      >
-        &#10092;
-      </div>
-    </div>
-  );
-};
+    return (
+        <>
+            <div
+                className={`absolute sm:w-[408px] h-screen bg-white z-[1001] transition-transform duration-200 ${
+                    sidePanel ? '-translate-x-0' : '-translate-x-full'
+                } w-full`}
+            >
+                <div className="overflow-scroll scrollbar-width-thin h-screen">
+                    {selectedGpx ? (
+                        <div>
+                            <div className="flex pt-2 pl-2">
+                                <button
+                                    onClick={() => setSelectedGpx(() => null)}
+                                >
+                                    <div className="flex text-blue-400">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            strokeWidth={1.5}
+                                            stroke="currentColor"
+                                            className="size-6"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                d="M15.75 19.5 8.25 12l7.5-7.5"
+                                            />
+                                        </svg>
+                                        Back
+                                    </div>
+                                </button>
+                                <div className="font-bold ml-2">
+                                    {selectedGpx.properties.name}
+                                </div>
+                            </div>
+                            <TrackDetail selectedGpx={selectedGpx} map={map} />
+                        </div>
+                    ) : (
+                        <div>
+                            <button
+                                className="sm:hidden pt-2 pl-2"
+                                onClick={() => setSidePanel(() => false)}
+                            >
+                                <div className="flex text-blue-400">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth={1.5}
+                                        stroke="currentColor"
+                                        className="size-6"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M15.75 19.5 8.25 12l7.5-7.5"
+                                        />
+                                    </svg>
+                                    Back
+                                </div>
+                            </button>
+                            <TracksList tracks={tracks} />
+                        </div>
+                    )}
+                </div>
+                <div
+                    className={`sm:flex items-center absolute left-full sm:left-[408px] top-1/2 w-8 h-16 rounded-r-lg bg-white z-[401] hidden`}
+                    onClick={() => setSidePanel((v) => !v)}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className={`size-6 transition-all ease-in-out duration-200 ${
+                            !sidePanel && 'transform -scale-x-100'
+                        }`}
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                        />
+                    </svg>
+                </div>
+            </div>
+            <div
+                onClick={() => setSidePanel((v) => !v)}
+                className="bg-white left-2 top-2 absolute z-[401] rounded-full p-2 drop-shadow-lg sm:hidden"
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                    />
+                </svg>
+            </div>
+        </>
+    )
+}
 
-export default SidePanel;
+export default SidePanel
